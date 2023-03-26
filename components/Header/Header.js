6e229db1bb8a71581d362470bbb4d1e731ad7617
PORT-CPEREZ\Cristian Pérez <cperez@labelgrup.com>
2023-03-26 14:40:27 +0200

    assets

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,11 @@
 import MLOGO from './../Layout/components/assets/logos/m-markt-logo.png'
+import Image from 'next/image'
 
 const Header = () => {
   return (
         <div className="w-full bg-primary text-white p-4">
-            <div className="flex justify-between items-center">
-                <img src={MLOGO} alt="logo" />
+            <div className="flex justify-between items-center bg-slate-300">
+                <Image width={100} height={100} src={MLOGO} alt="logo" />
                 <p>erp system</p>
              </div>
 
